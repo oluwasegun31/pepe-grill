213c637e91d70ab9cc514570bba3bdd328d81721
oluwasegun31 <oluwasegun1412@gmail.com>
2024-04-05 14:22:30 +0100

    Feat: Added Btn for game

---
+++ src/components/Features.jsx
@@ -0,0 +1,75 @@
+export default function Features() {
+  return (
+    <section className="w-full flex flex-col justify-start items-start py-6">
+      <p className="text-secondary md:text-6xl sm:text-4xl text-3xl font-semibold mb-12 sm:p-0 px-2">
+        Features
+      </p>
+      <section className="w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 max-w-[1240px] mx-auto mb-12">
+        <div className="bg-primary flex flex-col gap-4 justify-center items-center sm:h-[350px] h-[300px] p-4 text-center text-main hover:text-primary hover:bg-secondary transition-all duration-300 shadow-md">
+          <p className="sm:text-4xl text-3xl font-semibold">Sizzling Speed</p>
+          <p className="sm:text-lg text-base font-medium leading-tight">
+            Built on Solana for unparalleled transaction speeds and grill-like
+            heat
+          </p>
+        </div>
+        <div className="bg-primary flex flex-col gap-4 justify-center items-center sm:h-[350px] h-[300px] p-4 text-center text-main hover:text-primary hover:bg-secondary transition-all duration-300 shadow-md">
+          <p className="sm:text-4xl text-3xl font-semibold">
+            Community-Centric
+          </p>
+          <p className="sm:text-lg text-base font-medium leading-tight">
+            Decisions made through community governance. Your voice, your token.
+          </p>
+        </div>
+        <div className="bg-primary flex flex-col gap-4 justify-center items-center sm:h-[350px] h-[300px] p-4 text-center text-main hover:text-primary hover:bg-secondary transition-all duration-300 shadow-md">
+          <p className="sm:text-4xl text-3xl font-semibold">Limited Supply</p>
+          <p className="sm:text-lg text-base font-medium leading-tight">
+            With a capped token supply, we're keeping it exclusive to the grill
+            circle.
+          </p>
+        </div>
+        <div className="bg-primary flex flex-col gap-4 justify-center items-center sm:h-[350px] h-[300px] p-4 text-center text-main hover:text-primary hover:bg-secondary transition-all duration-300 shadow-md">
+          <p className="sm:text-4xl text-3xl font-semibold">
+            Engagement Rewards
+          </p>
+          <p className="sm:text-lg text-base font-medium leading-tight">
+            Holders can earn rewards through engagement, grilling up some
+            delicious token benefits.
+          </p>
+        </div>
+        <div className="bg-primary flex flex-col gap-4 justify-center items-center sm:h-[350px] h-[300px] p-4 text-center text-main hover:text-primary hover:bg-secondary transition-all duration-300 shadow-md">
+          <p className="sm:text-4xl text-3xl font-semibold">
+            Eco-Friendly Gril
+          </p>
+          <p className="sm:text-lg text-base font-medium leading-tight">
+            We're committed to sustainable practices, ensuring a green approach
+            to our grilling.
+          </p>
+        </div>
+      </section>
+      <section className="w-full flex justify-center items-center my-12">
+        <a href="">
+          <button className="capitalize p-4 bg-secondary text-main sm:text-4xl text-2xl font-semibold -skew-x-12 outline-none">
+            come gamble
+          </button>
+        </a>
+      </section>
+      <section className="w-full flex flex-col justify-center items-center text-center divide-primary divide-y-2">
+        <p className="text-secondary md:text-3xl text-2xl leading-tight font-medium py-2">
+          Join the "Pepe Grill" Community
+        </p>
+        <p className="py-2 text-primary sm:text-xl text-lg font-normal md:w-[700px] w-full">
+          Be a part of our grill community! Hold, trade, and participate in the
+          "Pepe Grill" ecosystem. Follow us for updates, community events, and
+          the latest grill gossip.
+        </p>
+      </section>
+      <section className="bg-secondary sm:p-4 p-2 md:rounded-3xl rounded-xl mt-12">
+        <p className="text-main sm:text-xl text-lg leading-tight font-medium text-center">
+          Pepe Grill" isn't just a token; it's a movement. A celebration of
+          crypto culture, communal strength, and, of course, a good grill. So
+          grab your apron, and let's get grilling!
+        </p>
+      </section>
+    </section>
+  );
+}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 export default function Hero() {
   return (
-    <section className="w-full min-h-[80vh] flex flex-col justify-center items-center text-center gap-6">
-      <p className="text-secondary md:text-7xl text-5xl font-semibold">
+    <section className="w-full min-h-[80vh] flex flex-col justify-center items-center text-center gap-6 pepeImg">
+      <section className="absolute top-0 left-0 w-full h-full bg-main/85 z-10"></section>
+      <p className="text-secondary md:text-7xl text-5xl font-semibold z-20">
         Welcome to "Pepe-Grill,"
       </p>
-      <p className="w-[90%] text-primary font-normal md:text-xl text-lg">
+      <p className="w-[90%] text-primary font-normal md:text-xl text-lg z-20">
         where we're cooking up the freshest approach to cryptocurrency. Inspired
         by the vibrant and ever-evolving meme culture, "Pepe Grill" isn't just
         another digital asset—it's a community token that brings people together
